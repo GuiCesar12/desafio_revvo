1c8a311bbfeadceec16216f635e87a6aa9551f48
GuiCesar12 <guilherme.cesar.reis@hotmail.com>
2025-01-28 16:38:22 -0300

    fix views

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -9,8 +9,6 @@ use League\Plates\Extension\Asset;
 abstract class Controller {
     public static function view(string $view,array $data = []){
         $viewsPath = dirname(__FILE__,3). "/views";
-        var_dump($viewsPath.DIRECTORY_SEPARATOR.$view .".php");
-        die;
         if(!file_exists($viewsPath.DIRECTORY_SEPARATOR.$view .".php")){
 
             throw new Exception("View inexistente");
